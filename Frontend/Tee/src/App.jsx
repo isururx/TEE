5ea51e01c34275e5b6123cf245a90e9fe1f08e91
shashiD365 <shashidivyanga232004@gmail.com>
2026-08-13 10:29:11 +0530

    restore dark mode toggle

--- Frontend/Tee/src/App.jsx
+++ Frontend/Tee/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Login from "./auth/Login.jsx";
 import DiseaseDetection from "./disease/DiseaseDetection.jsx";
 import StateAnalytics from "./analytics/stateAnalytics.jsx";
